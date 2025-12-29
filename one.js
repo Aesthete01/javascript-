e27011a5d50109e6c89625bc351f17cbc9c0f187
Aesthete01 <arpitsevariya@gmail.com>
2025-12-29 22:24:21 +0530

    my js recap of control flow

---
+++ one.js
@@ -0,0 +1,25 @@
+// today i studied if else condition or how to use them in js 
+//few operators ; ==, ===(datatype),!=, >=,<=, >,< ,&&,||
+// var in global scope 
+// if(balance >700) console.log("test")
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+// numer=number ; string=string 
+// ++++ falsy values 
+// 0 , false , null , undefined , 0n , -0 , BigInt , "", NaN 
+// ++++ truthy values 
+// [] , "0" , 'false' ," " , {}, function(){}  
+// condition to check array 
+// if (usermail.length ===0) {
+// console.log("array is empty") 
+ const emptyobj = {}
+
+if (Object.keys(emptyobj).length ){
+    console.log("object is empty "); 
+}
