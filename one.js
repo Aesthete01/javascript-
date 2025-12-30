de714779c458e213a3c90e0c9517def3db09dfa2
Aesthete01 <arpitsevariya@gmail.com>
2025-12-30 22:03:44 +0530

    NCO in js

--- one.js
+++ one.js
@@ -18,8 +18,25 @@
 // condition to check array 
 // if (usermail.length ===0) {
 // console.log("array is empty") 
- const emptyobj = {}
+//  const emptyobj = {}
 
-if (Object.keys(emptyobj).length ){
-    console.log("object is empty "); 
-}
+// if (Object.keys(emptyobj).length ){
+//     console.log("object is empty "); 
+// }
+
+// nullish coalescing operator (??): null undefined 
+
+let num1; 
+// num1 = 5 ?? 10 
+ 
+//num1 = null ?? 10 
+//num1 = undefined ?? 10 
+num1 = undefined ?? 10 ?? 15 
+console.log(num1)
+
+// terniary operator 
+
+// condition ? true : false 
+
+const icecreamprice = 100 
+icecreamprice  <= 70 ? console.log("less than 70") : console.log("more then 70 ")
