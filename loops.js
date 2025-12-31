872eaab8c1e5badc468aaa480519508347a21502
Aesthete01 <arpitsevariya@gmail.com>
2025-12-31 22:09:25 +0530

    loops in js

--- loops.js
+++ loops.js
@@ -11,10 +11,10 @@ for (let index = 1; index <=  10; index++) {
 }
 
 for (let index = 0; index <= 10 ; index++) {
-//console.log(`outer loop value: ${index}`)
+console.log(`outer loop value: ${index}`)
     for (let p = 0; p  <= 10;  p++) {
-       // console.log(`inner loop value ${p} and inner loop${index} `)
-       //console.log(index + '*' + p + '=' + index*p) 
+        console.log(`inner loop value ${p} and inner loop${index} `)
+       console.log(index + '*' + p + '=' + index*p) 
         
     }
     
@@ -24,11 +24,11 @@ let myarray = ["apple", "banana", "guava"]
 
 for (let index = 0; index < myarray.length; index++) {
     const element = myarray[index]; 
-    //console.log(element);
+    console.log(element);
 }
 
 
-// break and continue 
+ //break and continue 
 
 for (let index = 0; index <= 10 ; index++) {
     if (index == 5) {
@@ -48,6 +48,27 @@ for (let index = 1; index <= 10;  index++) {
     
 }
 
+//while loop ++++
 
+let index = 0 
+while (index <= 10 ) {
+   // console.log(index)
+    index = index +2 
+}
+
+let myarrayy = [ "happy", "sam", "varun"]
+let arr = 0 
+ while (arr < myarray.length) {
+    //console.log(`value is ${myarray[arr]}`);
+    arr = arr+ 1 
+ }
+
+ // do while loop 
+
+ let score = 110 
+ do {
+    console.log(`score is ${score }`);
+    score = score + 10 
+ } while (score <= 100 );
 
 
